a8c77c8664d352ff795ecd48e5cf3c974fcc473b
post <postwear25@gmail.com>
2024-12-22 02:24:57 +0300

    Complete Traffic Light app with static constraits and switch algorithm

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var yellowLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
+    
+    @IBOutlet var proccessButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.layer.cornerRadius = redLabel.frame.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.width / 2
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
+        
+        proccessButton.layer.cornerRadius = 10
+
     }
-
-
+    
+    @IBAction func proccessButtonTapped() {
+        if proccessButton.currentTitle == "Start" {
+            proccessButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLabel.alpha = lightIsOff
+            redLabel.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLabel.alpha = lightIsOff
+            yellowLabel.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLabel.alpha = lightIsOff
+            greenLabel.alpha = lightIsOn
+            currentLight = .red
+            
+        }
+  
+    }
 }
 
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
+//test for github
